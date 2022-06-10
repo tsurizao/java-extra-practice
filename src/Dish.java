e559dacd50b88eba6724ea7050305634e1be37d0
Chase Martinez <chasemartinez22@gmail.com>
2022-06-10 13:43:48 -0500

    refactor mini exercise to allow for empty object creation

--- src/Dish.java
+++ src/Dish.java
@@ -3,12 +3,16 @@ public class Dish {
     String nameOfDish;
     boolean wouldRecommend;
 
-    public Dish(int costInCents, String nameOfDish, boolean wouldRecommend){
+    public Dish (){
+    };
+
+    public Dish(int costInCents, String nameOfDish, boolean wouldRecommend) {
         this.costInCents = costInCents;
         this.nameOfDish = nameOfDish;
         this.wouldRecommend = wouldRecommend;
     }
-    public void printSummary(){
-        System.out.printf("Cost: $0.%s \nName: %s \nRecommended: %s\n", costInCents, nameOfDish, wouldRecommend);
+
+    public void printSummary() {
+        System.out.printf("Cost: %d \nName: %s \nRecommended: %b\n", costInCents, nameOfDish, wouldRecommend);
     }
 }
--- src/DishTest.java
+++ src/DishTest.java
@@ -1,7 +1,13 @@
 public class DishTest {
     public static void main(String[] args) {
+        Dish dish = new Dish();
         Dish dish1 = new Dish(79, "a mouthwatering slice of crispy bacon", true);
         Dish dish2 = new Dish(99, "searing hot ghost pepper popper", true);
+        System.out.println();
+        dish.nameOfDish = "ice cold glass of water";
+        dish.costInCents = 50;
+        dish.wouldRecommend = true;
+        dish.printSummary();
         dish1.printSummary();
         dish2.printSummary();
     }
