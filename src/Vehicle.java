b70c708a60a30fb472242498f13a9ede08611624
Chase Martinez <chasemartinez22@gmail.com>
2022-06-14 14:11:59 -0500

    Add inheritence mini-exercises

---
+++ src/Vehicle.java
@@ -0,0 +1,24 @@
+public class Vehicle {
+
+    private String name;
+
+    public Vehicle(){
+
+    }
+
+    public Vehicle(String name){
+        this.name = name;
+    }
+
+    public void makeNoise() {
+        System.out.println("Vroom vroom");
+    }
+
+    public void getName(){
+        System.out.println(this.name);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
---
+++ src/VehicleTest.java
@@ -0,0 +1,9 @@
+public class VehicleTest {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setName("Chevelle");
+        car.getName();
+        car.makeNoise();
+    }
+}
