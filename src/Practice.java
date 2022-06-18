e196a6d2f543a0f46fbb2b3d2beb129e4e0fb57a
Chase Martinez <chasemartinez22@gmail.com>
2022-06-18 16:25:17 -0500

    Add java practice assessment with exercise 2 completion

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,11 @@
+// 1. Create a class called Practice with a main method.
 public class Practice {
     public static void main(String[] args) {
 
+    }
+
+    //2. Create a public static method in the Practice class called *subtractTen* that takes one integer argument and returns that number minus 10.
+    public static int subtractTen(int num) {
+        return num - 10;
     }
 }
