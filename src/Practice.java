cdaa93d6455f47fd78fc620862d82742a69bbe26
Chase Martinez <chasemartinez22@gmail.com>
2022-06-18 16:36:46 -0500

    Add java practice assessment with exercise 3 coompletion

--- src/Practice.java
+++ src/Practice.java
@@ -1,11 +1,27 @@
 // 1. Create a class called Practice with a main method.
 public class Practice {
     public static void main(String[] args) {
+        //exercise tests
 
+        //2
+        System.out.println("Expected 2: " + subtractTen(12));
+
+        //3
+        int[] myIntArray = new int[]{5, 7, 2, 23, 13};
+        System.out.println("Expected 10.0: " + average(myIntArray));
     }
 
     //2. Create a public static method in the Practice class called *subtractTen* that takes one integer argument and returns that number minus 10.
     public static int subtractTen(int num) {
         return num - 10;
+    }
+
+    //3. Create a public static method *average* in the Practice class that takes an array of integers and returns their average as a double.
+    public static double average(int[] array) {
+        double total = 0;
+        for (int arrNum : array) {
+            total += arrNum;
+        }
+        return total / array.length;
     }
 }
