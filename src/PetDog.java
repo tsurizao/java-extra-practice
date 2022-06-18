75d849d83c20469b6eb91d90a1031838bd100c9d
Chase Martinez <chasemartinez22@gmail.com>
2022-06-18 16:52:58 -0500

    Add java practice assessment exercise 8 and 9 completion

--- src/PetDog.java
+++ src/PetDog.java
@@ -1,6 +1,25 @@
 //6. Create a PetDog class that inherits from Pet.
 public class PetDog extends Pet {
 
-//    7. In PetDog, create a boolean instance variable, trained.
+    //    7. In PetDog, create a boolean instance variable, trained.
     private boolean trained;
+
+    //    8. Write a constructor for PetDog that sets its name, type, and trained properties.
+    public PetDog() {
+    }
+
+    public PetDog(String name, String type, boolean trained) {
+        this.setName(name);
+        this.setType(type);
+        this.trained = trained;
+    }
+
+    public void setTrained(boolean isTrained) {
+        this.trained = isTrained;
+    }
+
+    //9. Write an instance method in PetDog  called isTrained that returns the boolean value of the trained property.
+    public boolean isTrained() {
+        return trained;
+    }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -9,6 +9,13 @@ public class Practice {
         //3
         int[] myIntArray = new int[]{5, 7, 2, 23, 13};
         System.out.println("Expected 10.0: " + average(myIntArray));
+
+        //4 & 5
+        Pet pet = new Pet("Polly", "Parakeet");
+        System.out.println(pet.getName() + " is a " + pet.getType());
+
+        PetDog sammy = new PetDog("Mimi", "Miniature Schnauzer", true);
+        System.out.println(sammy.getName() + " is a " + sammy.getType() + " and it is " + sammy.isTrained() + " that it is trained.");
     }
 
     //2. Create a public static method in the Practice class called *subtractTen* that takes one integer argument and returns that number minus 10.
