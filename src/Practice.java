54bdc750f03e4f137e9b2f294963309bba4691f8
Chase Martinez <chasemartinez22@gmail.com>
2022-06-18 17:20:41 -0500

    Add java practice assessment exercise 12 completion

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 // 1. Create a class called Practice with a main method.
 public class Practice {
     public static void main(String[] args) {
@@ -14,9 +16,19 @@ public class Practice {
         Pet pet = new Pet("Polly", "Parakeet");
         System.out.println(pet.getName() + " is a " + pet.getType());
 
+        //testing for 7, 8, 9
         PetDog sammy = new PetDog("Mimi", "Miniature Schnauzer", true);
         System.out.println(sammy.getName() + " is a " + sammy.getType() + " and it is " + sammy.isTrained() + " that it is trained.");
         System.out.println(sammy.snuggle());
+
+        //12
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        arrList.add(5);
+        arrList.add(6);
+        arrList.add(7);
+        arrList.add(8);
+        arrList.add(9);
+        System.out.println("Expected output 15, 18, 21, 24, 27: " + multiplyAll(3, arrList));
     }
 
     //2. Create a public static method in the Practice class called *subtractTen* that takes one integer argument and returns that number minus 10.
@@ -31,5 +43,11 @@ public class Practice {
             total += arrNum;
         }
         return total / array.length;
+    }
+
+    //    12. In your Practice class, create a static method named multiplyAll. It should accept an integer and an ArrayList of integers. It should take the ArrayList argument and multiply all its elements by the value of the integer argument.
+    public static ArrayList<Integer> multiplyAll(int num, ArrayList<Integer> arrList) {
+        arrList.replaceAll(integer -> integer * num);
+        return arrList;
     }
 }
