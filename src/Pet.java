361506918e6ddc7367844cee0d0ea74e97aef182
Chase Martinez <chasemartinez22@gmail.com>
2022-06-18 16:41:04 -0500

    Add java practice assessment exercise 5 completion

--- src/Pet.java
+++ src/Pet.java
@@ -2,4 +2,29 @@
 public class Pet {
     private String name;
     private String type;
+
+    //    5. Create getters and setters for the properties as well as a constructor that takes two strings and sets the name and type.
+    public Pet() {
+    }
+
+    public Pet(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
