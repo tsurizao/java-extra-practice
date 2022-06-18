959df276ccee053236c2b9eef9a3e0e9da6a70d4
Chase Martinez <chasemartinez22@gmail.com>
2022-06-18 16:43:31 -0500

    Add java practice assessment exercise 7 completion

--- src/PetDog.java
+++ src/PetDog.java
@@ -1,3 +1,6 @@
 //6. Create a PetDog class that inherits from Pet.
 public class PetDog extends Pet {
+
+//    7. In PetDog, create a boolean instance variable, trained.
+    private boolean trained;
 }
