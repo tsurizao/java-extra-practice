eecf329c7f3b9957368233d2221b907f3ca5a8cc
Chase Martinez <chasemartinez22@gmail.com>
2022-07-08 16:52:04 -0500

    practice using ArrayList

--- src/CollectionsPractice.java
+++ src/CollectionsPractice.java
@@ -1,22 +1,18 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CollectionsPractice {
     public static void main(String[] args) {
-        HashMap<String, String> usernames = new HashMap<>();
-        usernames.put("Chase", "tsurizao");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(0, 45);
+        numbers.add(32);
+        System.out.println(numbers);
 
-        System.out.println(usernames);
-        usernames.putIfAbsent("Zach", "zgulde");
-        usernames.putIfAbsent("Alfredo", "noonchio");
-        System.out.println(usernames);
+        System.out.println(numbers.size());
+        System.out.println(numbers.get(1));
 
-        usernames.remove("Zach");
-        System.out.println(usernames);
-
-        usernames.replace("Alfredo", "bruh");
-        System.out.println(usernames);
-
-        usernames.clear();
-        System.out.println(usernames.isEmpty());
+        System.out.println(numbers.indexOf(32));
+        System.out.println(numbers.indexOf("dog"));
     }
 }
